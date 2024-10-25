699bc50a2b72b8f8a931345ec656bdfbe9ee6d86
FragStudios <jaydenxwragg@outlook.com>
2024-10-25 20:57:46 +0100

    Intercepted the request to remove stuff we don't need before loading the page to hopefully receive a quicker response

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ async function getStreamerGame(streamerName) {
 
     try 
     {
+        await page.setRequestInterception(true); // This is required to intercept the request in order to attempt a quicker response
+        page.on("request", (req) => { // Intercept the request before attempting to load the page
+            if (["image", "media", "font"].includes(req.resourceType())) {
+                req.abort(); // Block images/videos and font from the request to decrease loading times
+            }
+            else
+            {
+                req.continue();
+            }
+        })
+
         await page.goto(streamerURL); // Go to the streamers page
         await page.waitForSelector(pageElementToLookFor); // Wait for the game name to be available
         const gameName = await page.$eval(pageElementToLookFor, element => element.textContent); // Fetch the game name when it's available to us
